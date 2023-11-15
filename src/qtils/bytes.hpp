b97df7c2aece7a3989741d8a9ec9a03bba11e8d8
turuslan <turuslan.devbox@gmail.com>
2023-11-15 13:09:42 +0300

    hex, unhex, bytestr
    Signed-off-by: turuslan <turuslan.devbox@gmail.com>

---
+++ src/qtils/bytes.hpp
@@ -0,0 +1,20 @@
+/**
+ * Copyright Quadrivium LLC
+ * All Rights Reserved
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#pragma once
+
+#include <array>
+#include <cstdint>
+#include <span>
+#include <vector>
+
+namespace qtils {
+  using Bytes = std::vector<uint8_t>;
+  template <size_t N>
+  using BytesN = std::array<uint8_t, N>;
+  using BytesIn = std::span<const uint8_t>;
+  using BytesOut = std::span<uint8_t>;
+}  // namespace qtils
---
+++ src/qtils/unhex.hpp
@@ -0,0 +1,83 @@
+/**
+ * Copyright Quadrivium LLC
+ * All Rights Reserved
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#pragma once
+
+#include <boost/algorithm/hex.hpp>
+#include <qtils/bytes.hpp>
+#include <qtils/enum_error_code.hpp>
+#include <qtils/outcome.hpp>
+
+namespace qtils {
+  enum class UnhexError {
+    UNEXPECTED_0X,
+    REQUIRED_0X,
+    ODD_LENGTH,
+    TOO_SHORT,
+    TOO_LONG,
+    NON_HEX,
+  };
+  Q_ENUM_ERROR_CODE(UnhexError) {
+    using E = decltype(e);
+    switch (e) {
+      case E::UNEXPECTED_0X:
+        return "UNEXPECTED_0X";
+      case E::REQUIRED_0X:
+        return "REQUIRED_0X";
+      case E::ODD_LENGTH:
+        return "ODD_LENGTH";
+      case E::TOO_SHORT:
+        return "TOO_SHORT";
+      case E::TOO_LONG:
+        return "TOO_LONG";
+      case E::NON_HEX:
+        return "NON_HEX";
+    }
+    abort();
+  }
+
+  template <typename T = Bytes>
+  outcome::result<T> unhex(std::string_view s) {
+    if (s.starts_with("0x")) {
+      return UnhexError::UNEXPECTED_0X;
+    }
+    if (s.size() % 2 != 0) {
+      return UnhexError::ODD_LENGTH;
+    }
+    auto count = s.size() / 2;
+    T t;
+    if constexpr (requires(T t) { t.resize(size_t{}); }) {
+      t.resize(count);
+    } else {
+      if (count < t.size()) {
+        return UnhexError::TOO_SHORT;
+      }
+      if (count > t.size()) {
+        return UnhexError::TOO_LONG;
+      }
+    }
+    try {
+      boost::algorithm::unhex(s.begin(), s.end(), t.begin());
+    } catch (const boost::algorithm::non_hex_input &) {
+      return UnhexError::NON_HEX;
+    }
+    return t;
+  }
+
+  template <typename T = Bytes>
+  outcome::result<T> unhex0x(std::string_view s, bool optional_0x = false) {
+    if (s.starts_with("0x")) {
+      s.remove_prefix(2);
+    } else if (not optional_0x) {
+      return UnhexError::REQUIRED_0X;
+    }
+    return unhex<T>(s);
+  }
+
+  inline auto operator""_unhex(const char *c, size_t s) {
+    return unhex(std::string_view{c, s}).value();
+  }
+}  // namespace qtils
