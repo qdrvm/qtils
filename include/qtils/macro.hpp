29def1202b8b97c03beabb914a9037a16d0d2081
Vladimir Shcherba <abrehchs@gmail.com>
2024-11-13 16:39:56 +0300

    Fix unwrap macro

--- include/qtils/macro.hpp
+++ include/qtils/macro.hpp
@@ -16,15 +16,19 @@
 
 #define QTILS_UNWRAP_NAME QTILS_UNIQUE_NAME(unwrap_res_)
 
-#define QTILS_UNWRAP(decl, expr)                       \
-  auto QTILS_UNWRAP_NAME = (expr);                     \
-  if (!QTILS_UNWRAP_NAME) {                            \
-    return std::unexpected(QTILS_UNWRAP_NAME.error()); \
-  }                                                    \
-  decl = QTILS_UNWRAP_NAME.value();
+#define QTILS_UNWRAP_3(unwrap_res, decl, expr)  \
+  auto unwrap_res = (expr);                     \
+  if (!unwrap_res) {                            \
+    return std::unexpected(unwrap_res.error()); \
+  }                                             \
+  decl = unwrap_res.value();
 
-#define QTILS_UNWRAP_void(expr)                        \
-  auto QTILS_UNWRAP_NAME = (expr);                     \
-  if (!QTILS_UNWRAP_NAME) {                            \
-    return std::unexpected(QTILS_UNWRAP_NAME.error()); \
+#define QTILS_UNWRAP(decl, expr) QTILS_UNWRAP_3(QTILS_UNWRAP_NAME, decl, expr)
+
+#define QTILS_UNWRAP_void_2(unwrap_res, expr)   \
+  auto unwrap_res = (expr);                     \
+  if (!unwrap_res) {                            \
+    return std::unexpected(unwrap_res.error()); \
   }
+
+#define QTILS_UNWRAP_void(expr) QTILS_UNWRAP_void_2(QTILS_UNWRAP_NAME)
