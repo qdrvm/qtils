846584819f9a0f207061663eb868d84a9cc98a73
Harrm <abrehchs@gmail.com>
2024-11-21 16:21:33 +0300

    Fix from review

--- src/qtils/macro/unwrap.hpp
+++ src/qtils/macro/unwrap.hpp
@@ -8,27 +8,24 @@
 
 /**
  *  QTILS_UNWRAP - attemps to extract the value from the provided std::expected
- *      expression and assign it to the provided declaration, returns error if fails.
- *  QTILS_UNWRAP_void - same as QTILS_UNWRAP, but no assignment (for std::expected<void, E>).
+ *      expression and assign it to the provided declaration, returns error if
+ * fails. QTILS_UNWRAP_void - same as QTILS_UNWRAP, but no assignment (for
+ * std::expected<void, E>).
  */
- 
+
 #include <qtils/macro/common.hpp>
 
 #define QTILS_UNWRAP_NAME QTILS_UNIQUE_NAME(unwrap_res_)
 
-#define QTILS_UNWRAP_3(unwrap_res, decl, expr)  \
-  auto unwrap_res = (expr);                     \
-  if (!unwrap_res) {                            \
-    return std::unexpected(unwrap_res.error()); \
-  }                                             \
-  decl = unwrap_res.value();
-
-#define QTILS_UNWRAP(decl, expr) QTILS_UNWRAP_3(QTILS_UNWRAP_NAME, decl, expr)
-
-#define QTILS_UNWRAP_void_2(unwrap_res, expr)   \
-  auto unwrap_res = (expr);                     \
-  if (!unwrap_res) {                            \
-    return std::unexpected(unwrap_res.error()); \
+#define QTILS_UNWRAP_void_2(res, expr)   \
+  auto &&res = (expr);                   \
+  if (res.has_error()) {                 \
+    return std::unexpected(res.error()); \
   }
 
+#define QTILS_UNWRAP_3(res, out, expr) \
+  QTILS_UNWRAP_void_2(res, expr);      \
+  out = std::move(res).value()
+
 #define QTILS_UNWRAP_void(expr) QTILS_UNWRAP_void_2(QTILS_UNWRAP_NAME, expr)
+#define QTILS_UNWRAP(out, expr) QTILS_UNWRAP_3(QTILS_UNWRAP_NAME, out, expr)
