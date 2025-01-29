b0eea288c5b2c42e884cc338f2086a9252d382f4
Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>
2025-01-29 18:51:11 +0800

    feature: FinalAction
    Signed-off-by: Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>

---
+++ src/qtils/final_action.hpp
@@ -0,0 +1,68 @@
+/**
+ * Copyright Quadrivium LLC
+ * All Rights Reserved
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#pragma once
+
+#include <cstdint>
+#include <optional>
+#include <utility>
+
+#include <qtils/option_take.hpp>
+
+namespace qtils {
+
+  template <typename F>
+  struct FinalAction {
+    FinalAction() = delete;
+    FinalAction(FinalAction &&func) = delete;
+    FinalAction(const FinalAction &func) = delete;
+    FinalAction &operator=(FinalAction &&func) = delete;
+    FinalAction &operator=(const FinalAction &func) = delete;
+
+    FinalAction(F &&func) : func(std::forward<F>(func)) {}
+
+    ~FinalAction() {
+      func();
+    }
+
+    // To prevent an object being created on the heap
+    void *operator new(size_t) = delete;            // standard new
+    void *operator new(size_t, void *) = delete;    // placement new
+    void *operator new[](size_t) = delete;          // array new
+    void *operator new[](size_t, void *) = delete;  // placement array new
+
+   private:
+    F func;
+  };
+
+  template <typename F>
+  FinalAction(F &&) -> FinalAction<F>;
+
+  template <typename F>
+  struct MovableFinalAction {
+    MovableFinalAction() = delete;
+    MovableFinalAction(MovableFinalAction &&func)
+        : func{qtils::optionTake(func.func)} {}
+    MovableFinalAction(const MovableFinalAction &func) = delete;
+    MovableFinalAction &operator=(MovableFinalAction &&func) = delete;
+    MovableFinalAction &operator=(const MovableFinalAction &func) = delete;
+
+    MovableFinalAction(F &&func) : func(std::forward<F>(func)) {}
+
+    ~MovableFinalAction() {
+      if (func.has_value()) {
+        func->operator()();
+      }
+    }
+
+   private:
+    std::optional<F> func;
+  };
+
+  template <typename F>
+  MovableFinalAction(F &&) -> MovableFinalAction<F>;
+
+}  // namespace qtils
