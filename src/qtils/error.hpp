9a64dfd6ed0226dec29805aa89d4c713a6f81d9f
Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>
2025-02-18 19:32:27 +0800

    feature: optional use demangled full type of category in format of error
    Signed-off-by: Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>

--- src/qtils/error.hpp
+++ src/qtils/error.hpp
@@ -10,6 +10,16 @@
 
 #include <fmt/format.h>
 
+#include <memory>
+#include <string>
+
+#ifdef __has_include
+#if __has_include(<cxxabi.h>)
+#include <cxxabi.h>
+#define HAS_DEMANGLING
+#endif
+#endif
+
 namespace boost::system {
   class error_code;
 }  // namespace boost::system
@@ -26,18 +36,49 @@ namespace qtils {
 
 template <>
 struct fmt::formatter<std::error_code> {
-  static constexpr auto parse(format_parse_context &ctx) {
-    return ctx.begin();
+  // Presentation format:
+  // 'l' - long (print category, value, and message)
+  // 's' - short (print message only)
+  char presentation =
+#ifdef FORMAT_ERROR_WITH_FULLTYPE
+      'l';
+#else
+      's';
+#endif
+
+  constexpr auto parse(format_parse_context &ctx) -> decltype(ctx.begin()) {
+    auto it = ctx.begin(), end = ctx.end();
+    if (it != end && (*it == 's' || *it == 'l')) {
+      presentation = *it++;
+    }
+    if (it != end && *it != '}') {
+      throw format_error("invalid format");
+    }
+    return it;
   }
-  static auto format(const auto &error, format_context &ctx) {
-    return fmt::format_to(ctx.out(),
-        "{}({}) {}",
-        error.category().name(),
-        error.value(),
-        error.message());
+
+  [[nodiscard]] auto format(
+      const std::error_code &error, format_context &ctx) const {
+    if (presentation == 'l') {
+      std::string_view category = error.category().name();
+#ifdef HAS_DEMANGLING
+      int status = 0;
+      std::unique_ptr<char, void (*)(void *)> demangled(
+          abi::__cxa_demangle(category.data(), nullptr, nullptr, &status),
+          std::free);
+      if (status == 0) {
+        category = demangled.get();
+      }
+#endif
+      return fmt::format_to(
+          ctx.out(), "{}({}) {}", category, error.value(), error.message());
+    }
+    return fmt::format_to(ctx.out(), "{}", error.message());
   }
 };
 
 template <>
 struct fmt::formatter<boost::system::error_code>
     : fmt::formatter<std::error_code> {};
+
+#undef HAS_DEMANGLING
